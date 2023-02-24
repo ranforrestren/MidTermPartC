00380ff77a5f19960efea3d2dc94ab7d582fe355
Ran Ren <ranforrestren@gmail.com>
2023-02-24 12:21:48 -0500

    Add first_name class to branch

---
+++ src/midtermexam_versiona_extensioncode/ran.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author sorai
+ */
+
+public class ran {
+    // Here are some comments
+    // This is an empty class - it doesn't do anything, just to demonstrate branching in git!
+}
